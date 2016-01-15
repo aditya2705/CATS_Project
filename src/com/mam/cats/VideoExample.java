8afcea212369a221c35070f8479f88c4a34b3616
Aditya Rathi <adityarathi95@gmail.com>
2016-01-15 19:04:51 +0530

    Video playing implementation - added vlcj dependency - vlcj version 3.9.0 - working on both .mp4 & .avi

---
+++ src/com/mam/cats/VideoExample.java
@@ -0,0 +1,47 @@
+package com.mam.cats;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.Timer;
+
+import uk.co.caprica.vlcj.binding.LibVlc;
+import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
+import uk.co.caprica.vlcj.runtime.RuntimeUtil;
+
+import com.sun.jna.Native;
+import com.sun.jna.NativeLibrary;
+
+public class VideoExample {
+
+    private final EmbeddedMediaPlayerComponent mediaPlayerComponent;
+
+    public static void main(final String[] args) {
+        NativeLibrary.addSearchPath(RuntimeUtil.getLibVlcLibraryName(), "C:\\Program Files\\VideoLAN\\VLC");
+        Native.loadLibrary(RuntimeUtil.getLibVlcLibraryName(), LibVlc.class);
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new VideoExample(args);
+            }
+        });
+    }
+
+    private VideoExample(String[] args) {
+        JFrame frame = new JFrame("vlcj Tutorial");
+
+        mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
+
+        frame.setContentPane(mediaPlayerComponent);
+
+        frame.setLocation(100, 100);
+        frame.setSize(500, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        mediaPlayerComponent.getMediaPlayer().playMedia("C:\\Users\\Aditya Rathi\\Videos\\Veep\\Veep - The Complete Season 1 [HDTV]\\Veep.S01E02.mp4");
+
+    }
+}
